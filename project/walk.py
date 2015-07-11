3de7aefe813c84b945ec855ff8ec350e07da5415
Madelyn Freed <madelyn.freed@gmail.com>
2015-07-11 15:14:47 -0400

    added includes string function that takes a string from the command line

--- project/walk.py
+++ project/walk.py
@@ -1,13 +1,29 @@
 import os
+from sys import argv
+
+script, regex = argv
 
 def is_log_file(filename):
 	return True
 
+def includes_string(pathname, regex):
+	reading_file = open(pathname, 'r')
+
+	if regex in reading_file.read():
+		return True
+	else:
+		return False
+
 f = []
-for (dirpath, dirnames, filenames) in os.walk("."):
+for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
 	full_path_names = [os.path.join(dirpath,name) for name in filenames if is_log_file(name)]
 	f.extend(full_path_names)
 
-print f
+# print f
+
+matched_files = [pathname for pathname in f if includes_string(pathname, regex)]
+
+print matched_files
 
 	
+
