9e8d1d083cef524939c4a64b3d6f9d18d28f32aa
Madelyn Freed <madelyn.freed@gmail.com>
2015-07-20 11:00:47 -0400

    removed test directories

--- tests/rebuildgrep_tests.py
+++ tests/rebuildgrep_tests.py
@@ -4,10 +4,27 @@ from nose.tools import *
 from rebuildgrep import walk
 
 def setup():
-	
+	print "setup"	
 
 def teardown():
 	print "TEAR DOWN"
 
 def test_basic():
 	print "I RAN"
+
+def test_includes_string():
+	pass
+
+def test_is_log_file():
+	x = walk.Recurse('_')
+	assert(x.is_log_file("bloggy.py"))
+
+def test_is_not_log_file():
+	x = walk.Recurse('_')
+	assert_false(x.is_log_file("blgy.py"))
+
+def test_find_matching_filenames():
+	pass
+
+def test_find_matched_files():
+	pass
